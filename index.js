f99a2a71cd366f05413fa23e52099e505473d62f
David Spear <davidspear@Davids-MacBook-Pro.local>
2021-06-17 16:42:50 -0400

    task 1, started task 2

--- index.js
+++ index.js
@@ -42,8 +42,26 @@ class Airplane {
   */
   
  class Person {
-    
+    constructor(name,age){
+      this.name = name;
+      this.age = age;
+      this.stomach = [];
+    }
+    eat(edible){
+      if(this.stomach.length < 10){
+        this.stomach.push(edible);
+      }
+    }
+    poop(){
+      this.stomach = [];
+    }
+    toString(){
+      return `${this.name}, ${this.age}`;
+    }
   }
+
+
+
   
   /*
     TASK 2
@@ -60,6 +78,19 @@ class Airplane {
   */
   
  class Car {
+   constructor(model, milesPerGallon, tank, odometer){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+   }
+   fill(gallons){
+     this.tank += gallons;
+   }
+   drive(distance){
+     this.odometer += distance;
+   }
+
     
   }
   
